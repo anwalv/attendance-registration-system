18a5aceb28208dd396334a46538ed13db8e44dcb
Adriana Hryhoryshyna <ahryhoryshyna@kse.org.ua>
2026-09-23 23:40:12 +0300

    added is_actie for users

--- KSEAttendApp/backend/db/schema.sql
+++ KSEAttendApp/backend/db/schema.sql
@@ -4,6 +4,7 @@ CREATE TABLE users (
     name VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL UNIQUE,
     password_hash VARCHAR(255) NOT NULL,
+    active BOOLEAN NOT NULL DEFAULT TRUE,
     created_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
 );
 
